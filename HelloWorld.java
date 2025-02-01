dc393c86ee02319b2f356ac2ed34c1cee0a57660
Bright Gee Varghese <bvarghese@miu.edu>
2025-02-01 17:38:21 -0600

    Changed from PayPal to GPay

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ class HelloWorld {
         System.out.println("Life is beautiful.");
         System.out.println("IT job is exciting..");
         System.out.println("Trying a crazy feature");
-        System.out.println("PayPal gateway feature");
+        System.out.println("GPay gateway feature");
     }
 }
