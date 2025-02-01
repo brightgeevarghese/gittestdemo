b0d9cca0de9721fa03596e79dd1367443cd90cd2
Bright Gee Varghese <bvarghese@miu.edu>
2025-02-01 17:19:42 -0600

    added crazy feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Life is beautiful.");
         System.out.println("IT job is exciting..");
+        System.out.println("Trying a crazy feature");
     }
 }
