d203ab6533d36584b28478282195c147455aea36
Bright Gee Varghese <bvarghese@miu.edu>
2025-02-01 17:31:47 -0600

    Revert "Added PayPal feature"
    This reverts commit 9535ae9d5a0b49b5a717c5bff7ea45409e01a3fa.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,5 @@ class HelloWorld {
         System.out.println("Life is beautiful.");
         System.out.println("IT job is exciting..");
         System.out.println("Trying a crazy feature");
-        System.out.println("PayPal gateway feature");
     }
 }
